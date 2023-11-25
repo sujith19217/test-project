f9cc028554b4713864552ff512d1e089c9660a70
root <root@ip-172-31-1-241.ap-south-1.compute.internal>
2023-11-25 16:23:57 +0000

    terrform code

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+
+}
+
+resource "aws_instance" "one" {
+ami = "ami-0d92749d46e71c34c"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.test-sg.id]
+tags = {
+Name = "worker-node"
+environment = "dev"
+}
+}
+
+
---
+++ security.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "test-sg" {
+name = "pro-sg"
+description = "all,ssh,tcp"
+
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "ssh"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
