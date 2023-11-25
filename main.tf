febb9c56f85e45e8e4fd6bb405cab982d4be16a1
root <root@ip-172-31-1-241.ap-south-1.compute.internal>
2023-11-25 17:11:10 +0000

    keypair

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "one" {
 ami = "ami-0d92749d46e71c34c"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.test-sg.id]
+key_name = "aws"
 tags = {
 Name = "worker-node"
 environment = "dev"
