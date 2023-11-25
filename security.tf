0bf34b3c3a1b14089d87eea3c3b857083d953b7b
root <root@ip-172-31-1-241.ap-south-1.compute.internal>
2023-11-25 16:57:18 +0000

    sec-updated

--- security.tf
+++ security.tf
@@ -10,13 +10,6 @@ cidr_blocks = ["0.0.0.0/0"]
 }
 
 ingress {
-from_port = 22
-to_port = 22
-protocol = "ssh"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
-ingress {
 from_port = 0
 to_port = 0
 protocol = "-1"
